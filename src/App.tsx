1a5e5ffdd53c1b90d004585cd8f2be9aef5ea06b
Revaz Tamoev <revaztamoev522@gmail.com>
2024-06-07 19:08:34 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+        <h1 className='text-3xl text-yellow-500 '>Hello, World!</h1>
+      </div>
+   
+    </>
+  )
+}
+
+export default App
