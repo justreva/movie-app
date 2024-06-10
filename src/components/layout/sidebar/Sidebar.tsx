4432d5a37fda0be5227392d14066b879d85f221e
Revaz Tamoev <revaztamoev522@gmail.com>
2024-06-10 17:26:26 +0300

    change something

--- src/components/layout/sidebar/Sidebar.tsx
+++ src/components/layout/sidebar/Sidebar.tsx
@@ -1,6 +1,6 @@
 import { FC, useState } from "react"
 import { Layout} from 'antd';
-
+import { HomeOutlined } from "@ant-design/icons";
 const Sidebar: FC = () => {
 	const [collapsed, setCollapse] = useState(false);
 
@@ -10,6 +10,14 @@ const Sidebar: FC = () => {
 			<img src="../../../../public/logo.svg" alt="logo" />
 			<h1>{collapsed ? "" : "SVault"}</h1>
 		</div>
+
+		<ul className="sidebar-list">
+			<li className="sidebar-list__element">
+			<HomeOutlined />
+			<h1>Home</h1>
+			</li>
+		</ul>
+
 		</Layout.Sider>
 	)
 }
