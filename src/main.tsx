aab985706086460260bcf47222bee25b5a2bff06
Revaz Tamoev <revaztamoev522@gmail.com>
2024-10-15 20:32:45 +0300

    add the mutation and set login to the localStorage

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import "./index.css";
 import App from "./App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "react-query";
 
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </Provider>
 );
-
--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,11 +1,18 @@
 import "./Auth.css";
+import { useMutation } from "react-query";
+import { mutationLogin } from "./mutation";
 const Auth = () => {
+  const {data, mutate} = useMutation({mutationKey: ["login"], mutationFn: mutationLogin})
+  const handleLogin = async () => {
+    await mutate()
+    localStorage.setItem("guest_session_id", data.guest_session_id)
+  }
   return (
     <div className="auth-container">
       <div className="auth-container__content">
         <h1 className="auth-title">Welcome to the SVault!</h1>
         <h2 className="auth-desc">Press on the button to log in as a Guest</h2>
-        <button className="auth-btn">
+        <button className="auth-btn" onClick={handleLogin}>
           <img src="/logo.svg" alt="" />
         </button>
       </div>
