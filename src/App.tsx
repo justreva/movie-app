6047546a1afb6c984c1475dff837d500d5b8e37f
Revaz Tamoev <revaztamoev522@gmail.com>
2024-10-20 19:11:11 +0300

    Did some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/home";
-
-
+import "./index.css"
 const App = () => {
   return (
     <>
       <Router>
-				<Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/movies" element={<h1>Movies</h1>} />
-          <Route path="/serials" element={<h1>Serials</h1>} />
-          <Route path="/favorites" element={<h1>Favorites</h1>} />
-        </Routes>
+        <Navbar />
+        <div className="container-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/movies" element={<h1>Movies</h1>} />
+            <Route path="/serials" element={<h1>Serials</h1>} />
+            <Route path="/favorites" element={<h1>Favorites</h1>} />
+          </Routes>
+        </div>
       </Router>
     </>
   );
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,27 +3,25 @@ import "./Home.css";
 const Home = () => {
   return (
     <main className="content">
-      <div className="content-wrap">
-        <div className="title">
-          Welcome back, <i>User</i>. Here’s what you can watch…
+      <div className="title">
+        Welcome back, <i>User</i>. Here’s what you can watch…
+      </div>
+
+      <div className="recent-films">
+        <div className="recent-films__title">
+          <a href="#" className="">
+            Recent films released
+          </a>
         </div>
 
-        <div className="recent-films">
-          <div className="recent-films__title">
-            <a href="#" className="">
-              Recent films released
-            </a>
-          </div>
-
-          <ul className="recent-films-grid">
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-            <li>5</li>
-            <li>6</li>
-          </ul>
-        </div>
+        <ul className="recent-films-grid">
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+          <li>5</li>
+          <li>6</li>
+        </ul>
       </div>
     </main>
   );
