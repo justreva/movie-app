a13c508122c6ca56447134a79059c42066f9dd3d
Revaz Tamoev <revaztamoev522@gmail.com>
2024-10-27 22:08:36 +0300

    Need to change details page

--- src/pages/MovieDetails.tsx/MovieDetails.tsx
+++ src/pages/MovieDetails.tsx/MovieDetails.tsx
@@ -10,30 +10,35 @@ const MovieDetails = () => {
   );
   const movie = data?.data || [];
   console.log(movie);
+  const getYear = (date: string) => {
+    return new Date(date).getFullYear();
+  };
+
   return (
     <>
-      <div className="h-[400px] left-0 right-0 top-0 relative border border-bc">
-        <div className="overlay-film-cover"></div>
+     <div className="overlay-film-right"></div>
+     <div className="overlay-film-left"></div>
+     
+      <div className="h-[400px] border border-bc">
         <img
           src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
           alt="backdrop"
           className="w-full h-full object-cover"
         />
+       
       </div>
-      <div className="container mx-auto">
-        <div className="absolute top-[40%] flex">
+      <div className="container mx-auto ">
+        <div className="">
           <img
             src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
             alt=""
             className="max-w-[250px] border border-bc rounded-lg "
           />
-          <div className="about-movie ml-[100px]">
-            <h1 className="text-secondary text-3xl font-medium">
+          <div className="about-movie flex items-center">
+            <h1 className="text-secondary text-2xl font-medium">
               {movie.title}
             </h1>
-            <h2>
-              {}
-            </h2>
+            <h2 className="text-xl ml-2 text-secondary">{getYear(movie.release_date)}</h2>
           </div>
         </div>
       </div>
