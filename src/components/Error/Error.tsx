d717c73ea1efdca1a93c878617bb0ab93660182a
Revaz Tamoev <revaztamoev522@gmail.com>
2024-11-01 13:46:36 +0300

    Micro fix

--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -2,7 +2,7 @@ const Error = () => {
   return (
     <div className="container mx-auto text-center mt-[300px]">
       <h1 className="text-active  text-5xl font-medium">Error</h1>
-			<h2 className="text-description text-xl italic mt-2">Sorry Something went wring. Please try again</h2>
+			<h2 className="text-description text-xl italic mt-2">Sorry something went wrong. Please try again</h2>
     </div>
   );
 };
